2629c62d994aa969033b9bb583d7653796f37cf7
Josue Marin <josue.marin@usap.edu>
2024-04-11 14:48:35 -0600

    Refactor App.js to add swipe functionality and improve loading experience

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import { useSwipeable } from 'react-swipeable';
 import { ThreeDots } from 'react-loader-spinner';
 import Flashcard from './Flashcard';
 import useQuestions from './useQuestions';
@@ -19,16 +20,33 @@ const App = () => {
     }, 3000); // Adjust the countdown time as needed
   };
 
+  const handleSwipe = (direction) => {
+    // Increment or decrement the current question index based on the swipe direction
+    if (direction === 'Right' && currentQuestionIndex < questions.length - 1) {
+      setCurrentQuestionIndex(currentQuestionIndex + 1);
+    } else if (direction === 'Left' && currentQuestionIndex > 0) {
+      setCurrentQuestionIndex(currentQuestionIndex - 1);
+    }
+  };
+
+  const swipeHandlers = useSwipeable({
+    onSwipedLeft: () => handleSwipe('Left'),
+    onSwipedRight: () => handleSwipe('Right'),
+  });
+
   return (
     <div className="app">
       {loading ? (
-        <ThreeDots type="ThreeDots" color="#00BFFF" height={80} width={80} />
+        <Loader type="ThreeDots" color="#00BFFF" height={80} width={80} />
       ) : questions.length > 0 ? (
-        <Flashcard
-          question={questions[currentQuestionIndex].question}
-          options={questions[currentQuestionIndex].options}
-          answer={questions[currentQuestionIndex].answer_official}
-        />
+        <div {...swipeHandlers}>
+          <Flashcard
+            key={currentQuestionIndex}
+            question={questions[currentQuestionIndex].question}
+            options={questions[currentQuestionIndex].options}
+            answer={questions[currentQuestionIndex].answer_official}
+          />
+        </div>
       ) : (
         <FileDropzone onFileAccepted={handleFileAccepted} />
       )}
