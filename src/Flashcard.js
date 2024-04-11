b06bf94cef496849fef56bf16ec78fccd1a64203
Josue Marin <josue.marin@usap.edu>
2024-04-11 16:45:01 -0600

    Update background color and font size in App.css

--- src/Flashcard.js
+++ src/Flashcard.js
@@ -3,25 +3,26 @@ import './Flashcard.css';
 
 const Flashcard = ({ question, options, answer }) => {
     const [flipped, setFlipped] = useState(false);
+
     const flipCard = () => {
         setFlipped(!flipped);
-      };
+    };
     
-      return (
+    return (
         <div className="flashcard" onClick={flipCard}>
-          <div className={`card ${flipped ? 'flipped' : ''}`}>
-            <div className="front">
-              <h1>{question}</h1>
-              {Object.entries(options).map(([key, value]) => (
-                <p key={key}>{key}: {value}</p>
-              ))}
+            <div className={`card ${flipped ? 'flipped' : ''}`}>
+                <div className="front">
+                    <h1>{question}</h1>
+                    {Object.entries(options).map(([key, value]) => (
+                    <p key={key}>{key}: {value}</p>
+                    ))}
+                </div>
+                <div className="back">
+                    <p>Answer: {answer}</p>
+                </div>
             </div>
-            <div className="back">
-              <p>Answer: {answer}</p>
-            </div>
-          </div>
         </div>
-      );
-    };    
-  
+    );
+};    
+
 export default Flashcard;
